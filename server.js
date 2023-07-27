a2c9b91a301d23f741a8f0104edf455d4b15aea5
Jan Donyada <14304570+jandonyada@users.noreply.github.com>
2023-07-27 11:45:54 +0800

    abstracted db name

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: 'known_initialisms',
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
 });
