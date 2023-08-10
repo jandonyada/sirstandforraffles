b3f0131065771481c1d10b9cde56a2a5f779974d
Jan Donyada <14304570+jandonyada@users.noreply.github.com>
2023-08-10 23:00:14 +0800

    many updates

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
-// This code waits for the DOM to be fully loaded and ready for manipulation
+// Wait for DOM to fully load. Btw, I'm using vanilla JavaScript here (lol) to manipulate the DOM and making HTTP requests
 document.addEventListener('DOMContentLoaded', () => {
-  // It selects the HTML element with the ID 'searchForm' and assigns it to the 'searchForm' variable
+  // Selects the HTML element with ID 'searchForm' and assigns it to the 'searchForm' variable
   const searchForm = document.getElementById('searchForm');
+  const searchButton = document.getElementById('searchButton'); // Assuming the search button has an ID of 'searchButton'
 
-  // It adds an event listener to the 'submit' event of the 'searchForm'
+  // Listens to 'submit' event of 'searchForm'
   searchForm.addEventListener('submit', event => {
-    // Prevents the default form submission behavior, which would cause a page reload
+    // Form submission causes a page reload (which we don't want), so this stops that
     event.preventDefault();
 
-    // Calls the function 'searchAcronym()' to handle the form submission and perform the desired action
+    // Calls the function 'searchAcronym()' to handle form submissions
     searchAcronym();
   });
+
+  // Add an input event listener to the search input
+  const acronymInput = document.getElementById('acronymInput');
+  acronymInput.addEventListener('input', () => {
+    // Disable the search button if the input is empty or contains only white spaces
+    searchButton.disabled = acronymInput.value.trim() === '';
+  });
+
+  // Initial check and disable if the input is empty
+  searchButton.disabled = acronymInput.value.trim() === '';
 });
 
-
+// this defines the searchAcronym function, which the primary way the frontend passes each form submission for the backend to process
 function searchAcronym() {
+  // removes white spaces & converts entire string to uppercase (because the acronyms in the DB are all uppercase)
   const input = document.getElementById('acronymInput').value.trim().toUpperCase();
-  const firstWord = input.split(' ')[0]; // Retrieve the first word
+  // Splits string by space (which converts the string into an array of strings), then takes the first word with [0]. We'll use this to search the DB for acronyms.
+  const firstWord = input.split(' ')[0];
 
-  // Clear previous results
+  // Clears previous results by finding an HTML element with the `id` attribute `results` in the HTML doc, then sets the content to an empty string.
   document.getElementById('results').innerHTML = '';
 
-  // Call the API with the firstWord
+  // Call the API with the firstWord variable.
+  // fetch() initiates a HTTP GET request to the specified URL
+  // We use ${} convention to insert `firstWord` as the `acronym` query parameter, but also use `encodeURIComponent` to safely encode `firstWord` in case it contains special characters (which could cause the URL to not be valid.)
   fetch(`/api/search?acronym=${encodeURIComponent(firstWord)}`)
+    // This is part of what is called a 'promise chain', i.e. when the fetch() request is made, the `.then` methods are used to handle the responses.
+    // response => response.json() parses the response into JSON
     .then(response => response.json())
+    // Continuation of the promise chain. It takes the parsed JSON data from the previous step and processes it.
     .then(data => {
+      // `.found` checks if there's data in the response or not
       if (data.found) {
-        // Display table with results
+        // If acronym has match(es), then display a table with the results. Table because it's the easiest way to represent an array (there can be multiple hits for a given acronym)
         showResultsTable(data.results);
       } else {
-        // Display suggestion form
+        // If no match, display suggestion form, which allows users to suggest meaning to the unknown acronym. Actually, we should always allow a suggestion form, even if matches are found. Next time lah :p
         showSuggestionForm(firstWord);
       }
     })
+    // If for some reason got error whenever calling this function, the thrown error is printed onto the console. Can make it more verbose, but for now it just says 'Error'. 
     .catch(error => {
       console.error('Error:', error);
     });
 }
 
+// This is a function to display any results in table format.
 function showResultsTable(results) {
+  // Creates a new HTML <table> element. This will be the container for the results to be displayed in.
   const table = document.createElement('table');
+  // Column headers!
   const thead = table.createTHead();
+  // Inserts rows within the thead element 
   const row = thead.insertRow();
+  // Defining headers for the column headers
   const headers = ['Acronym', 'Meaning', 'Suggester'];
 
+  // Loops through the array of column headers above to create table header cells for each array item
   for (let header of headers) {
     const th = document.createElement('th');
     const text = document.createTextNode(header);
@@ -51,44 +77,63 @@ function showResultsTable(results) {
     row.appendChild(th);
   }
 
+  // Body section of the table. This is the thing that actually contains the results in rows.
   const tbody = table.createTBody();
 
+  // Loops through each item in `result`
   for (let result of results) {
+    // for each result, insert a new row into the table
     const row = tbody.insertRow();
+    // Destructure the `result` object, so that it can have properties that mirror the `known_initialisms` DB table: initialism, meaning, suggester!
     const { initialism, meaning, suggester } = result;
 
+    // For each item in the `result` array (that has now been destructured into initialism, meaning, suggester), create the cells in the row
     for (let value of [initialism, meaning, suggester]) {
+      // creates a new table cell within the row
       const cell = row.insertCell();
+      // Preps the text we need to put inside each cell, where `value` represents the current value of `initialism`, `meaning`, and `suggester`.
       const text = document.createTextNode(value);
+      // Adds the text to the cell as cell content.
       cell.appendChild(text);
     }
   }
-
+  // appends the generated table (from the showResultsTable function) as a child element of `results` (in the HTML).
   document.getElementById('results').appendChild(table);
 
-  // Clear input field
+  // Minor usability improvement to ensure that the input field is reset and ready for the user to perform another search without any leftover content from the previous search.
   document.getElementById('acronymInput').value = '';
 }
 
+// Function to display a suggestion form so users can suggest a meaning for an unknown acronym
 function showSuggestionForm(acronym) {
+  // creates a paragraph element that says....
   const suggestionText = document.createElement('p');
+  // ...this when there are no matches from the searchAcronym function
   suggestionText.innerText = `Sorry, no matches were found for ${acronym}. Can you provide a suggestion?`;
 
+  // creates a form
   const form = document.createElement('form');
 
   // Create the div for the name input and label
   const nameDiv = document.createElement('div');
-  nameDiv.className = 'input-div';  // Apply the CSS class
+  nameDiv.className = 'input-div';  // Apply the CSS class (line 60 of index.html)
 
+  // creates a new HTML <label> element and assigns it to the `nameLabel` variable
   const nameLabel = document.createElement('label');
+  // Set the content of the label to 'Your name'
   nameLabel.innerText = 'Your name';
+  // Assigns the CSS class `input-label` to the `label` element
   nameLabel.className = 'input-label';
 
+  // creates a new HTML <input> element, and assigns it to the `nameInput` variable.
   const nameInput = document.createElement('input');
+  // sets the input to a `text` type, indicating that the input field is meant for text input.
   nameInput.setAttribute('type', 'text');
   nameInput.setAttribute('placeholder', 'Enter your name');
+  // when you mouseover the input, it'll also show 'Your name'–just a smol ting
   nameInput.setAttribute('title', 'Your name');
   nameInput.required = true;
+  // Assigns the CSS class `input-field` to the `input` element, so you can style this element using CSS
   nameInput.className = 'input-field';
 
   // Append the name label and input to the name div
@@ -99,8 +144,10 @@ function showSuggestionForm(acronym) {
   const suggestionDiv = document.createElement('div');
   suggestionDiv.className = 'input-div';  // Apply the CSS class
 
+  // creates a new <label> HTML element and assigns it to the `suggestionLabel` variable
   const suggestionLabel = document.createElement('label');
   suggestionLabel.innerText = 'Your suggestion';
+  // same CSS class as nameLabel
   suggestionLabel.className = 'input-label';
 
   const meaningInput = document.createElement('input');
@@ -114,54 +161,61 @@ function showSuggestionForm(acronym) {
   suggestionDiv.appendChild(suggestionLabel);
   suggestionDiv.appendChild(meaningInput);
 
+  // creates a new button element to the HTML and assigns it the `submitButton` variable
   const submitButton = document.createElement('button');
   submitButton.innerText = 'Submit';
   submitButton.setAttribute('type', 'submit');
+  // disable it by default
   submitButton.disabled = true;
 
+  // disable the submit button if either name or meaning input fields are empty
   const enableSubmitButton = () => {
     submitButton.disabled = !(nameInput.value && meaningInput.value);
   };
 
+  // adds event listeners to both the `nameInput` element and the `meaningInput` element
   nameInput.addEventListener('input', enableSubmitButton);
   meaningInput.addEventListener('input', enableSubmitButton);
 
+  // adds an event listener to the `form` element, and the event being listened to is the `submit` event, which occurs when a user submits the form
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     submitSuggestion(acronym, nameInput.value.trim(), meaningInput.value.trim());
   });
 
-  form.appendChild(suggestionText);
-  form.appendChild(nameDiv);  // Append the name div
-  form.appendChild(suggestionDiv);  // Append the suggestion div
-  form.appendChild(submitButton);
+  form.appendChild(suggestionText); // Append suggestionText into the form
+  form.appendChild(nameDiv);  // Append the name div to the form
+  form.appendChild(suggestionDiv);  // Append the suggestion div to the form
+  form.appendChild(submitButton); // Append the submit button to the form
 
+  // gets a reference to the `results` element
   const container = document.getElementById('results');
-  container.appendChild(form);
+  container.appendChild(form); // appends the form to the `results` container
 }
 
+// This function handles the submission itself AND the response from the server.
 function submitSuggestion(acronym, suggester, meaning) {
-  // Send a POST request to the API
+  // Send a POST request to the API.
   fetch('/api/suggest', {
-    method: 'POST',
+    method: 'POST', // Method is POST since we're adding the suggestion to the table.
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json', // Request body is JSON
     },
-    body: JSON.stringify({ acronym: acronym, suggester: suggester, meaning: meaning }),
+    body: JSON.stringify({ acronym: acronym, suggester: suggester, meaning: meaning }), // to construct the body of the request, we use .stringify to convert the object (that contains acronym, suggester, meaning) into a JSON string.
   })
-    .then(response => response.json())
+    .then(response => response.json()) // parses the JSON data from the response
     .then(data => {
-      if (data.success) {
+      if (data.success) { // 
         const container = document.getElementById('results');
-        // Clear the container
+        // Clear the `results` container
         container.innerHTML = '';
         // Create and display the success message
         const successText = document.createElement('p');
-        successText.innerText = `Thank you for your suggestion. You suggested that ${acronym} means: ${meaning}.`;
-        container.appendChild(successText);
+        successText.innerText = `Thank you for your suggestion. You suggested that ${acronym} means: ${meaning}.`; // success message that includes the acronym and the suggested meaning by the user.
+        container.appendChild(successText); // append the success message to the `results` container
       }
     })
-    .catch(error => {
+    .catch(error => { // if any errors occur during the fetch or promise chain, we log the error message in the console.
       console.error('Error:', error);
     });
 }
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const { Pool } = require('pg');
-const cors = require('cors');
+require('dotenv').config() // load variables from .env file into the node environment
+const express = require('express'); // Using express.js framework here
+const { Pool } = require('pg'); // Using postgres, hence pg here. 
+const cors = require('cors'); // enables Cross-Origin Resource Sharing (CORS) using the `cors` middleware. Allows my server to handle requests coming from different frontend apps running on other domains. Idk, there was this error and it seems the way to fix it is by adding this line :shrug:. My guess is this would allow my server to respond to requests coming from cloudflare, render.com (not necessarily from sirstandforraffles.sg)
 
-const app = express();
+const app = express(); // creates an intance of the Express app
+// creates a connection pool to the PG DB. It's called pool because it manages a pool of database connections, which helps improve performance and scalability.
+
+console.log('db host', process.env.DB_HOST)
 const pool = new Pool({
+  // abstracted these to .env file, so that the app will use different variables based on which environment it's running on. For prod, it's entered in render.com
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
@@ -11,57 +16,60 @@ const pool = new Pool({
   port: 5432,
 });
 
-// Enable CORS
+// Enables CORS
 app.use(cors());
 
 // Define the API endpoint
-app.get('/api/search', (req, res) => {
-  const { acronym } = req.query;
+app.get('/api/search', (req, res) => { // /api/search is the GET request path; `req` is the request object; `res` is the response object
+  const { acronym } = req.query; // extracts the `acronym` from the request object i.e. `req` (see line 30 in script.js)
 
   // Query the database for exact matches
   pool.query(
     `SELECT * FROM known_initialisms WHERE initialism = $1`,
-    [acronym.toUpperCase()],
-    (error, results) => {
-      if (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal server error' });
+    [acronym.toUpperCase()], // actually the frontend already uppercases the string, so this is kinda redundant. but screw it, it works i don't want to touch.
+    (error, results) => { // it either returns an error or result(s)
+      if (error) { // if error
+        console.error('Error:', error); // print the error in the console
+        res.status(500).json({ error: 'Internal server error' }); // send 500 error as a response with 'Internal server error'
       } else {
-        if (results.rows.length > 0) {
-          // Return the matching rows
+        if (results.rows.length > 0) { // if got matches...
+          // Return the array of matching rows
           res.json({ found: true, results: results.rows });
         } else {
-          // No match found
-          res.json({ found: false, initialism: acronym });
+          // if got no matches...
+          res.json({ found: false, initialism: acronym }); // return found false and the acronym (so that the frontend can show it in the suggestionForm)
         }
       }
     }
   );
 });
 
+// Allows Express.js to serve static files from a directory. In my case, it serves my index.html that's in the root directory of my app.
+
 app.use(express.static('.'))
 
-app.use(express.json()); // It allows express to understand json payloads
+app.use(express.json()); // It allows express to understand json payloads. Frontend sends requests with JSON (see line 204 of script.js for example)
 
-app.post('/api/suggest', (req, res) => {
+app.post('/api/suggest', (req, res) => { // suggest endpoint for when no matches are found from the initial search.
   const { acronym, meaning, suggester } = req.body;
 
   // Insert the suggestion into the database
   pool.query(
-    `INSERT INTO known_initialisms (initialism, meaning, suggester) VALUES ($1, $2, $3)`,
-    [acronym.toUpperCase(), meaning, suggester],
+    `INSERT INTO known_initialisms (initialism, meaning, suggester) VALUES ($1, $2, $3)`, // values in the array of the second argument (next line) to be inserted in this order into column headers of the table.
+    [acronym.toUpperCase(), meaning, suggester], // i.e. value of `acronym.toUpperCase() --> initialism, value of `meaning` to `meaning`, and `suggester` to `suggester`.
     (error, results) => {
       if (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error('Error:', error); // print error in the console if shit happens
+        res.status(500).json({ error: 'Internal server error' }); // return error in the response body so frontend can display it to the user
       } else {
-        res.json({ success: true, initialism: acronym, meaning: meaning });
+        res.json({ success: true, initialism: acronym, meaning: meaning }); // if ok then return success true along with the added acronym and its meaning (we display it in the success message)
       }
     }
   );
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const port = process.env.PORT || 3000; // note that I haven't specify PORT in prod environment variables yet
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`); // references .env file when starting server. Logs it on console.
 });
